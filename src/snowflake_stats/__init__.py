ef230ad1e58e0be1865d37f0f8a2eabc3bd108ae
Hellow <Lars.Noack@outlook.de>
2023-01-25 18:39:54 +0100

    finished

--- src/snowflake_stats/__init__.py
+++ src/snowflake_stats/__init__.py
@@ -5,8 +5,6 @@ SNOWFLAKE_LOG_PATH = "../snow.log"
 relayed_data, restart_data = database.get_data(SNOWFLAKE_LOG_PATH)
 
 stats_obj = stats.Stats(relayed_data)
-print(str(stats_obj))
-
 
 def dump_markdown(markdown_path: str):
     stats.dump_markdown(markdown_path, stats_obj)
